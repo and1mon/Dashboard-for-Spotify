a4de89f8228242932f3eb4aea29d9bf9af5ad0dd
André Fürch <anfuerch.socials@gmail.com>
2023-02-07 20:50:11 +0100

    Add oauth configuration

---
+++ src/app/auth.config.ts
@@ -0,0 +1,16 @@
+import { AuthConfig } from 'angular-oauth2-oidc';
+
+export const authCodeFlowConfig: AuthConfig = {
+    issuer: 'https://accounts.spotify.com',
+    redirectUri: window.location.origin + "/redirect",
+    clientId: '9e49965bc899423a99adf1dc5f0e8d2d',
+    responseType: 'code',
+    scope: "user-read-private user-read-email",
+    showDebugInformation: true,
+    timeoutFactor: 0.01,
+    tokenEndpoint: "https://accounts.spotify.com/api/token",
+    disablePKCE: false,
+    oidc: false,
+    loginUrl: "https://accounts.spotify.com/authorize",
+    userinfoEndpoint: "https://api.spotify.com/v1/me"
+};
