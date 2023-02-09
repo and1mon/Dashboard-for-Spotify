e452038100ee060ce5c903776d9230a8a01efe2a
André <83401001+and1mon@users.noreply.github.com>
2023-02-09 20:22:29 +0100

    Add redirect after login for deeplinking

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,1 @@
 
-import { NgModule } from '@angular/core';
-import { BrowserModule, platformBrowser } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-
-@NgModule({
-  declarations: [],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: []
-})
-export class AppModule { }
-
-platformBrowser().bootstrapModule(AppModule);
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,19 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
-
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { provideHttpClient } from '@angular/common/http';
 
 import { AppComponent } from './app/app.component';
 import { provideOAuthClient } from 'angular-oauth2-oidc';
+import { provideRouter } from '@angular/router';
+import { ROUTES } from './app/app-routes';
 
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(),
-    provideOAuthClient()
+    provideOAuthClient(),
+    provideRouter(ROUTES)
   ]
 });
