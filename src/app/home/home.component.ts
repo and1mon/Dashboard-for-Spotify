ab3183c4eada1f806b2464df05df0c4b6d613112
André Fürch <anfuerch.socials@gmail.com>
2023-02-19 16:22:00 +0100

    Add home-view component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,24 +5,33 @@ import { ArtistObject, TrackObject } from 'src/libs/openapi';
 import { Subscription } from 'rxjs';
 import { ArtistsComponent } from '../artists/artists.component';
 import { TracksComponent } from '../tracks/tracks.component';
+import { HomeViewComponent } from './home-view/home-view.component';
+import { Store } from '@ngrx/store';
+import { AppState } from '../state/app.state';
+import { selectAllTracksShortTerm } from '../state/tracks/tracks.selector';
+import { loadTracks } from '../state/tracks/tracks.actions';
+import { selectAllArtistsShortTerm } from '../state/artists/artists.selector';
+import { loadArtists } from '../state/artists/artists.actions';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, ArtistsComponent, TracksComponent],
+  imports: [CommonModule, HomeViewComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit, OnDestroy {
+export class HomeComponent implements OnInit {
 
-  constructor(private spotifyService: SpotifyService) {
+  amount = 10;
 
-  }
+  tracks$ = this.store.select(selectAllTracksShortTerm(this.amount));
+  artists$ = this.store.select(selectAllArtistsShortTerm(this.amount));
+
+
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
-
-  }
-
-  ngOnDestroy(): void {
+    this.store.dispatch(loadTracks({ amount: this.amount, timeRange: "short_term" }));
+    this.store.dispatch(loadArtists({ amount: this.amount, timeRange: "short_term" }));
   }
 }
