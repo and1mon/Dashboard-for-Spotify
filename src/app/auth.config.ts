600f082f0151dc35e25cbf724202ad49170f0363
André Fürch <anfuerch.socials@gmail.com>
2023-02-20 14:04:03 +0100

    Add needed oauth scopes

--- src/app/auth.config.ts
+++ src/app/auth.config.ts
@@ -3,9 +3,9 @@ import { AuthConfig } from 'angular-oauth2-oidc';
 export const authCodeFlowConfig: AuthConfig = {
     issuer: 'https://accounts.spotify.com',
     redirectUri: window.location.origin + "/redirect",
-    clientId: '',
+    clientId: '9e49965bc899423a99adf1dc5f0e8d2d',
     responseType: 'code',
-    scope: "user-top-read",
+    scope: "user-top-read user-read-recently-played",
     showDebugInformation: true,
     timeoutFactor: 0.01,
     tokenEndpoint: "https://accounts.spotify.com/api/token",
