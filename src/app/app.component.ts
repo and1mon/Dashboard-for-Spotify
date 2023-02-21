2545c3df4235f0d8f339cbd62e0672ba8c88a8fa
André Fürch <anfuerch.socials@gmail.com>
2023-02-21 21:32:32 +0100

    Add footer and improve login screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,14 @@ import { RouterModule } from '@angular/router';
 import { RedirectComponent } from './redirect/redirect.component';
 import { HeaderComponent } from './atoms/header/header.component';
 import { ApiModule } from 'src/libs/openapi';
+import { FooterComponent } from './atoms/footer/footer.component';
 
 @Component({
   standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [CommonModule, RedirectComponent, RouterModule, ApiModule, HeaderComponent]
+  imports: [CommonModule, RedirectComponent, RouterModule, ApiModule, HeaderComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'UserDash - Stats for Spotify';
